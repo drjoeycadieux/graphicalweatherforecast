2538e8044bcb5ee2d86095c4bbaaf80c32afa0f1
Joey Cadieux <instructor.joeycadieux@gmail.com>
2026-09-10 23:35:31 +0000

    feat: simplify ProtectedEditorPage by removing authentication checks

--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -1,28 +1,7 @@
 "use client";
 
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-
-import { auth } from "@/lib/firebase";
 import WeatherEditor from "../weather-editor";
 
 export default function ProtectedEditorPage() {
-  const router = useRouter();
-
-  useEffect(() => {
-    if (!auth) {
-      router.replace("/public");
-      return;
-    }
-
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      if (!user) {
-        router.replace("/public");
-      }
-    });
-
-    return () => unsubscribe();
-  }, [router]);
-
   return <WeatherEditor mode="editor" />;
 }
